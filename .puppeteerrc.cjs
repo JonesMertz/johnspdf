0dc9e23934685e095204d9eb1d5edbd08e1978de
JonesMertz <jonasmertz@gmail.com>
2024-03-19 21:42:23 +0100

    more dirname

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,9 +1,12 @@
-const { join } = require('path');
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /**
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
     // Changes the cache location for Puppeteer.
-    cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+    cacheDirectory: path.join(__dirname, '.cache', 'puppeteer'),
 };
