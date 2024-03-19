4ac130d8d84544064453ea5ea2cf73071835be49
JonesMertz <jonasmertz@gmail.com>
2024-03-19 22:38:35 +0100

    Revert "more dirname"
    This reverts commit 0dc9e23934685e095204d9eb1d5edbd08e1978de.

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,12 +1,9 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const { join } = require('path');
 
 /**
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
     // Changes the cache location for Puppeteer.
-    cacheDirectory: path.join(__dirname, '.cache', 'puppeteer'),
+    cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
